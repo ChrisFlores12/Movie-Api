b717fb3ff7125dfcaa8dfb0a3ba7700b96d0d434
ChrisFlores12 <christianangeloflores1@gmail.com>
2025-07-14 23:00:21 -0400

    Created the update method for the ReviewService class and the endpoint for it as well in ReviewController.

--- src/main/java/dev/chris/moviesapi/ReviewController.java
+++ src/main/java/dev/chris/moviesapi/ReviewController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 import java.util.Map;
 
 
@@ -20,22 +20,27 @@ public class ReviewController {
     }
 
     //Create
-    @PostMapping
+
     //A POST request is needed to create a new review
     //The POST endpoint accepts a JSON payload, which is why we use the Map<String, String> payload
     //This PostMapping endpoint is connected to the /api/v1/reviews endpoint
 
+    @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<Review>(reviewService.createReview(payload.get("body"), payload.get("imdbId")), HttpStatus.CREATED);
+        return new ResponseEntity<>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
         //The payload is a map that contains the body of the review and the imdbId of the movie
         //Remember that CREATED returns a 201 status code
     }
 
     // Read
-    // Gets all reviews for a specific movie
-    @GetMapping("/{imdbId}")
-    public ResponseEntity<List<Review>> getReviewsByImdbId(@PathVariable String imdbId) {
-        return new ResponseEntity<List<Review>>(reviewService.getAllReviewsByImdbId(imdbId), HttpStatus.OK);
+    // This function is already implemented in the MovieController
+    // Remember that we can get all reviews for a specific movie by calling the getSingleMovieById method in the MovieController
+
+    // Update
+    @PutMapping("/update/{reviewId}")
+    public ResponseEntity<Review> updateReview(@PathVariable ObjectId reviewId, @RequestBody Map<String, String> payload) {
+        Review updatedReview = reviewService.updateReviewById(reviewId, payload.get("reviewBody"));
+        return new ResponseEntity<>(updatedReview, HttpStatus.OK); // Returns a 200 status code with the updated review
     }
 
     @DeleteMapping("/{reviewId}")
--- src/main/java/dev/chris/moviesapi/ReviewService.java
+++ src/main/java/dev/chris/moviesapi/ReviewService.java
@@ -9,8 +9,6 @@ import org.springframework.data.mongodb.core.query.Update;
 
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 
 @Service
 public class ReviewService {
@@ -21,15 +19,11 @@ public class ReviewService {
     //Need our movie repository to get the movie object
     private final MongoTemplate mongoTemplate; // we can use this to make a new dynamic query or, in other words, requests for info
 
-    private final MovieRepository movieRepository; //Use this grab movie objects from the database
-
-
     //Constructor injection for review repository and MongoTemplate
     //This is the preferred way to inject multiple dependencies because multiple constructors can confuse the Spring framework
-    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate, MovieRepository movieRepository) {
+    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate) {
         this.reviewRepository = reviewRepository;
         this.mongoTemplate = mongoTemplate;
-        this.movieRepository = movieRepository;
     }
 
     //Create
@@ -43,21 +37,7 @@ public class ReviewService {
         return review;
     }
 
-    // What if users want to see all the reviews for a specific movie?
-    // We can create a method that will search for a list of reviews based on the reviewIds in the Movie class
-
-    public List<Review> getAllReviewsByImdbId(String imdbId) {
-
-        // This finds the movie by imdbId and throws error exceptions if needed
-        Movie movie = movieRepository.findMovieByImdbId(imdbId).orElseThrow(
-                () -> new RuntimeException("Movie with " + imdbId + " not found")
-        );
-
-        return movie.getReviewIds();
-    }
-
     // Update Review
-    /*
     public Review updateReviewById(ObjectId reviewId, String body) {
         Review review = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new RuntimeException("Review with ID " + reviewId + " not found"));
@@ -66,8 +46,6 @@ public class ReviewService {
         review.setBody(body);
         return reviewRepository.save(review); // Save the updated review back to the database
     }
-
-     */
 
     // Delete
     public void deleteReviewById(ObjectId reviewId) {
